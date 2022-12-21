31fbc3639d2fe91da7679b178fe8e103f2ded7c2
Atte Koskivaara <attekoskivaara>
2022-12-21 14:11:42 +0200

    aws salaisuus lisätty

--- mysite/mysite/mysite/settings.py
+++ mysite/mysite/mysite/settings.py
@@ -9,7 +9,14 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from decouple import config
+import boto3
+from botocore.exceptions import ClientError
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
+def get_secret():
+
+    secret_name = "SECRET_KEY"
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
