1aff906f1d69f9bfc49b0ee47706a6639783c6e6
Atte Koskivaara <attekoskivaara>
2022-12-29 13:02:10 +0200

    ff

--- mysite/mysite/mysite/settings.py
+++ mysite/mysite/mysite/settings.py
@@ -76,7 +76,7 @@ PLOTLY_DASH = {
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = get_secret()
 
 # SECRET_KEY = config('SECRET_KEY')
 
