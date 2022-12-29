0549d981300a8c650c09a356758616acf98d2baf
Atte Koskivaara <attekoskivaara>
2022-12-29 09:48:21 +0200

    ...

--- mysite/mysite/mysite/settings.py
+++ mysite/mysite/mysite/settings.py
@@ -13,31 +13,6 @@ import boto3
 from botocore.exceptions import ClientError
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
-def get_secret():
-
-    secret_name = "SECRET_KEY"
-    region_name = "eu-north-1"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
-
-    # Decrypts secret using the associated KMS key.
-    secret = get_secret_value_response['SecretString']
-
-    # Your code goes here.
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -45,7 +20,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 PLOTLY_DASH = {
 
     # Route used for the message pipe websocket connection
-    "ws_route" :   "dpd/ws/channel",
+    "ws_routcde" :   "dpd/ws/channel",
 
     # Route used for direct http insertion of pipe messages
     "http_route" : "dpd/views",
@@ -73,8 +48,8 @@ PLOTLY_DASH = {
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#
-# SECRET_KEY = config('SECRET_KEY')
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
