1f8c835f337dac5b52e53a9f6fd4601dbfd19c84
hulicupteri <attekosk@gmail.com>
2023-01-09 12:10:32 +0200

    pythonpath updated

--- mysite/mysite/mysite/settings.py
+++ mysite/mysite/mysite/settings.py
@@ -228,7 +228,7 @@ STATIC_URL = '/static/'
 # Location of static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'www', 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
