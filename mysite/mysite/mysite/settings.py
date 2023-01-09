07b732ea4881861e938a84b2387f573de6fcf720
hulicupteri <attekosk@gmail.com>
2023-01-09 12:15:43 +0200

    lk

--- mysite/mysite/mysite/settings.py
+++ mysite/mysite/mysite/settings.py
@@ -223,12 +223,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 # Location of static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'www', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+
+STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
