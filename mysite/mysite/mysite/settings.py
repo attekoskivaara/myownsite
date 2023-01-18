152db8840986e3a450af2f10d1dedda7d7a88a67
Atte Koskivaara <attekoskivaara>
2023-01-18 12:48:32 +0200

    installing database

--- mysite/mysite/mysite/settings.py
+++ mysite/mysite/mysite/settings.py
@@ -167,13 +167,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
@@ -192,8 +185,7 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-# Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
