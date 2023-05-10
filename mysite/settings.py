a8e1c0f2d31911f2113cbedeb8738b0eaf16915d
hulicupteri <attekosk@gmail.com>
2023-05-10 18:02:19 +0300

    lisää

--- mysite/settings.py
+++ mysite/settings.py
@@ -295,4 +295,4 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 MEDIA_URL = '/media/'
 
 # Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
